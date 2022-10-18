36eaab6d72bb3197ea379016081f53f933e1892d
Rachit Khurana <70265590+rachit1601@users.noreply.github.com>
2022-10-19 00:21:10 +0530

    orders backend

--- orders/views.py
+++ orders/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render, redirect
-from orders.models import Order
+from orders.models import orders
 
 # Create your views here.
 
 def order(request):
-    orders=Order.objects.all()
-    return render(request, 'order.html', {'orders':orders})
+    orders_all=orders.objects.all()
+    return render(request, 'order.html', {'orders':orders_all})
 
 def order_detail(request, order_id):
-    order=Order.objects.get(id=order_id)
+    order=orders.objects.get(id=order_id)
     return render(request, 'order_detail.html', {'order':order})
 
 def new_order(request):
     if request.method == 'POST':
-        id = request.POST.get('id')
+        orders_no = orders.objects.count()
+        id = orders_no + 1
         date = request.POST.get('date')
         name = request.POST.get('name')
         enroll_no = request.POST.get('enroll_no')
@@ -31,7 +32,7 @@ def new_order(request):
         dupatta = request.POST.get('dupatta')
         total_clothes = request.POST.get('total_clothes')
         
-        order = Order(id=id, date=date, name=name, enroll_no=enroll_no, room=room, kurta=kurta, pyjama=pyjama, shirt=shirt, tshirt=tshirt, pant=pant, lower=lower, shorts=shorts, bedsheet=bedsheet, pillowcover=pillowcover, towel=towel, dupatta=dupatta, total_clothes=total_clothes)
+        order = order(id=id, date=date, name=name, enroll_no=enroll_no, room=room, kurta=kurta, pyjama=pyjama, shirt=shirt, tshirt=tshirt, pant=pant, lower=lower, shorts=shorts, bedsheet=bedsheet, pillowcover=pillowcover, towel=towel, dupatta=dupatta, total_clothes=total_clothes)
         order.save()
         return redirect('order_overview')
 
